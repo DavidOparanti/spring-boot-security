28747e0160c65d35192d7ab06ea2cfd3a974c56f
David Oparanti <david.oparanti@gmail.com>
2021-05-12 02:07:00 +0100

    Implement permission based authentication using antMatchers

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,8 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.ApplicationUserRole.ADMIN;
-import static com.example.demo.security.ApplicationUserRole.STUDENT;
+import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -27,9 +28,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index", "css/*", "js/*")
-                .permitAll()
+                .antMatchers("/", "index", "css/*", "js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -42,15 +48,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails davidUser = User.builder()
                 .username("david")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.grantedAuthorities())
                 .build();
 
         UserDetails kikeUser = User.builder()
                 .username("kike")
-                .password(passwordEncoder.encode("password"))
-                .roles(ADMIN.name())
+                .password(passwordEncoder.encode("passwords"))
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.grantedAuthorities())
                 .build();
 
-        return new InMemoryUserDetailsManager(davidUser, kikeUser);
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.grantedAuthorities())
+                .build();
+
+        return new InMemoryUserDetailsManager(davidUser, kikeUser, tomUser);
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+
+    List<Student> studentList = Arrays.asList(
+            new Student(1,"David Ayodeji"),
+            new Student(2, "Kikelomo Ayodeji"),
+            new Student(3, "Nifemi Ayodeji")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentList;
+    }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student) {
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable Integer studentId, @RequestBody Student student) {
+        System.out.printf("%s, %s%n", studentId, student);
+    }
+}
