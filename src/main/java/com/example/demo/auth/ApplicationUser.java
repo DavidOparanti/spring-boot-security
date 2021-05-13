179abd2e7481e3f4ab91b82336ee12d908fd5ec3
David Oparanti <david.oparanti@gmail.com>
2021-05-13 10:34:23 +0100

    Implement fake repository

--- src/main/java/com/example/demo/auth/ApplicationUser.java
+++ src/main/java/com/example/demo/auth/ApplicationUser.java
@@ -6,10 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
-public class AuthenticationUser implements UserDetails {
+public class ApplicationUser implements UserDetails {
 
-    private final List<? extends GrantedAuthority> grantedAuthorities;
+    private final Set<? extends GrantedAuthority> grantedAuthorities;
     private final String password;
     private final String username;
     private final boolean isAccountNonExpired;
@@ -17,17 +18,16 @@ public class AuthenticationUser implements UserDetails {
     private final boolean isCredentialsNonExpired;
     private final boolean isEnabled;
 
-    public AuthenticationUser(List<? extends GrantedAuthority> grantedAuthorities,
-                              String password,
-                              String username,
-                              boolean isAccountNonExpired,
-                              boolean isAccountNonLocked,
-                              boolean isCredentialsNonExpired,
-                              boolean isEnabled) {
-
-        this.grantedAuthorities = grantedAuthorities;
-        this.password = password;
+    public ApplicationUser(String username,
+                           String password,
+                           Set<? extends GrantedAuthority> grantedAuthorities,
+                           boolean isAccountNonExpired,
+                           boolean isAccountNonLocked,
+                           boolean isCredentialsNonExpired,
+                           boolean isEnabled) {
         this.username = username;
+        this.password = password;
+        this.grantedAuthorities = grantedAuthorities;
         this.isAccountNonExpired = isAccountNonExpired;
         this.isAccountNonLocked = isAccountNonLocked;
         this.isCredentialsNonExpired = isCredentialsNonExpired;
